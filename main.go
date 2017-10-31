5e9f2ceb9a030f0c79c494bd29b4700fec6787d5
Yikaros <jet113102@gmail.com>
2017-10-31 09:35:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -102,7 +102,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 							var menu []string
 							menu = strings.Split(list_array2[i], "$")
 							if menu[0] == itemname{
-								msg=msg + menu[1]
+								msg = menu[1]
 								break
 							}
 							i++
